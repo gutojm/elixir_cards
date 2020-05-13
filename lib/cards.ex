0727c7039fc66f9e03688b10d1548e5d24571637
Guto Meirelles <guto@iza.com.vc>
2020-05-13 17:32:00 -0300

    Until Session 4 end

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,16 +1,10 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Providing methods for creating and handling a deck of cards
   """
 
   @doc """
-  Creates a deck
-
-  ## Examples
-
-      iex> Cards.create_deck()
-      :world
-
+    Returns a list of strings representing a deck of playing cards
   """
 
   def create_deck do
@@ -41,9 +35,34 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck,"Ace of Spades")
+      true
+
+  """
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  @doc """
+    Divides a deck into a hand and the reaminder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
